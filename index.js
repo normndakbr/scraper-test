c1513dfff49b1a7dd11c16cf79f98ba670f6a035
normndakbr <normnd.akbr2@gmail.com>
2021-03-24 18:24:49 +0700

    get all articles url

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const request = require('request')
 const cheerio = require('cheerio')
 const source = "https://www.cermati.com/artikel"
+const articles = []
 
-request(source, function (err, res, body) {
-    if (err && res.statusCode !== 200) throw err;
-    //console.log(body);
-    let content = cheerio.load(body)
-
-    console.log(content('body').html());
+request(source, (err, response, content) => {
+    if (!err && response.statusCode == 200) {
+        let $ = cheerio.load(content)
+        $('div.article-list-item').each((i, el) => {
+            const url = $(el).find('a').attr('href')
+            console.log(url);
+        })
+    }
 });

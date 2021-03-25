be9d385ccc32a9cf05b15aa79fd60105cc8b588a
normndakbr <normnd.akbr2@gmail.com>
2021-03-25 08:34:44 +0700

    store all news links to an array

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const request = require('request')
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 const source = "https://www.cermati.com/artikel"
-const articles = []
+const links = []
 
 request(source, (err, response, content) => {
     if (!err && response.statusCode == 200) {
         let $ = cheerio.load(content)
         $('div.article-list-item').each((i, el) => {
             const url = $(el).find('a').attr('href')
-            console.log(url);
+            links.push(url)
         })
     }
+    console.log(links);
 });
